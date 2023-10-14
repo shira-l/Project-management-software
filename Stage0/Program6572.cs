4663aaa5027484342ec97baa435fea71127ef296
The user <Hand4c14@mbjcomp.org.il>
2023-10-15 01:53:08 +0300

    initial code+welcome function

---
+++ Stage0/Program6572.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace stage0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome6572();
+            Console.ReadKey();
+        }
+
+        private static void welcome6572()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
