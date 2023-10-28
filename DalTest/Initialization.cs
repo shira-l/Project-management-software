36f559003758c7244fbf0a980b43b28d5e9c19c9
shira-l <Hand4c14@mbjcomp.org.il>
2023-10-29 00:21:17 +0300

    current project

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -9,8 +9,65 @@ public static class Initialization
     private static IEngineer? s_dalEngineer; 
     private static ITask? s_dalTask;
     private static readonly Random s_rand = new();
-    private static void createDependency();
+    private static void createDependency()
+    {
+
+    }
+    private static void createEngineer()
+    {
+        string[] engineerNames =
+        {
+            "Ayala Levi",
+            "Bracha Klein",
+            "Gad Kohen",
+            "Dan Kanner",
+            "Hadar Fried",
+            "Vered Moses",
+            "Zisi Zander",
+            "Chaya Dror",
+            "Tova Levin",
+            "Yehudit Gamliel",
+            "Yael Solomon",
+            "Mali Kanal",
+            "Moshe Shif",
+            "Hadas Barzel",
+            "Tzipi Ziv",
+            "Ruth Kepler",
+            "Shaul Ben David",
+            "David Solomon",
+            "Chava Shor",
+            "shira Avraham",
+            "Avi Polack",
+            "Mina Kaplan",
+            "Benni Chen",
+            "Sara Gefner",
+            "Chaim Shitrit",
+            "chana Montag",
+            "Shoshi Miller",
+            "Yosef Chefetz",
+            "Tamar Frenckel",
+            "Aharon Negev",
+            "Tzvi Sason",
+            "Akiva Sokol",
+            "Zehev Segal",
+            "Rachel Gefen",
+            "Leah Mimran",
+            "Ariel Green",
+            "Pinchas Tzur",
+            "Nechama Meller",
+            "Dina Bagad",
+            "Natan Regev"
+        };
+        foreach (string name in engineerNames)
+        {
+            int id, MINid= 200000000, MAXid= 400000000;
+            do
+                id = s_rand.Next(MINid, MAXid);
+            while (s_dalEngineer!.Read(id) != null);
+
+        }
 
 
+    }
 
 }
