044e875c8b411d716c0937c745459a20f49f0bf1
YehuditGamliel <Hand4c5@mbjcomp.org.il>
2023-10-30 14:12:05 +0200

    new

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+
+
+         private static IStudent? s_dalStudent = new StudentImplementation(); //stage 1
+        private static ICourse? s_dalCourse = new CourseImlementation(); //stage 1
+        private static ILink? s_dalLinks = new LinkImplementation(); //stage 1
+
+         }
     }
+
 }
+         
