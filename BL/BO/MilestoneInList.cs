816653e210b4464a6d9998a8e5e2dadccae47fa8
shira-l <michalla37@gmail.com>
2023-12-29 01:08:27 +0200

    change

--- BL/BO/MilestoneInList.cs
+++ BL/BO/MilestoneInList.cs
@@ -6,6 +6,6 @@ public class MilestoneInList
     public int Id { get; init; }
     public string? Description { get; set; }
     public string? Alias { get; set; }
-    //public Status Status { get; init; }
+    public Status Status { get; init; }
     public double CompletionPercentage { get; set; }
 }
--- BL/BlApi/IEngineer.cs
+++ BL/BlApi/IEngineer.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace BlApi
+namespace BlApi;
+
+public interface IEngineer
 {
-    internal interface IEngineer
-    {
-    }
+    public int Create(BO.Engineer engineer);
+    public BO.Engineer? Read(int id);
+    public BO.Engineer? Read(Func<BO.Engineer, bool> filter);
+    public IEnumerable<BO.Engineer?> ReadAll(Func<BO.Engineer, bool>? filter = null);
+    public void Update(BO.Engineer engineer);
+    public void Delete(int id); 
 }
